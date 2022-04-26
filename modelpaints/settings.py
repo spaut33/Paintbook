9d9e8e8e60e1309cd5489d5788b3da3f34cf77b7
spaut33 <roman.petrakov@gmail.com>
2022-04-26 16:42:52 +0300

    fix security issues with settings.py

--- modelpaints/settings.py
+++ modelpaints/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wvzaum(zofq^%fchng(2+ywz7eh$=y@f6v@-i+eosgpnk@g@cz'
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
