d2bb9a37675d262f60117d46e74285e6215ed961
roman <roman.petrakov+git@gmail.com>
2022-05-04 18:23:46 +0300

    added django debug toolbar

--- modelpaints/settings.py
+++ modelpaints/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #  Django debug toolbar TODO: make it visible on serializer viewset (httpresponse)
     'debug_toolbar',
     'social_django',
     'tailwind',
