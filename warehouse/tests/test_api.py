1723f74883e5c89534406727fc538744cc3345d6
roman <roman.petrakov+git@gmail.com>
2022-05-24 00:51:57 +0300

    updated formatting in test_api.py

--- warehouse/tests/test_api.py
+++ warehouse/tests/test_api.py
@@ -1,10 +1,11 @@
 import json
 
-from django.db.models import Avg, Max
-from rest_framework.test import APITestCase
-from rest_framework import status
-from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db.models import Avg, Max
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
 from warehouse.models import Paint, Series, Manufacturer, UserPaint
 from warehouse.serializers import PaintSerializer
 
@@ -12,9 +13,13 @@ from warehouse.serializers import PaintSerializer
 class PaintsApiTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create(username='test_username')
-        self.user_staff = User.objects.create(username='admin_user', is_staff=True)
+        self.user_staff = User.objects.create(
+            username='admin_user', is_staff=True
+        )
         manufacturer = Manufacturer.objects.create(name='Test Manufacturer 1')
-        series = Series.objects.create(name='Test Series 1', manufacturer=manufacturer)
+        series = Series.objects.create(
+            name='Test Series 1', manufacturer=manufacturer
+        )
         self.paint_1 = Paint.objects.create(
             name='Test 1',
             catalog_number='600.00.00',
@@ -41,22 +46,27 @@ class PaintsApiTestCase(APITestCase):
         url = reverse('paint-list')
 
         response = self.client.get(url)
-        paints = Paint.objects.all().annotate(average_rating=Avg('userpaint__rating'),
-                                              max_rating=Max('userpaint__rating'))
+        paints = Paint.objects.all().annotate(
+            average_rating=Avg('userpaint__rating'),
+            max_rating=Max('userpaint__rating'),
+        )
         serializer_data = PaintSerializer(paints, many=True).data
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            serializer_data,
-            response.data
-        )
+        self.assertEqual(serializer_data, response.data)
         self.assertEqual(serializer_data[0]['average_rating'], None)
         self.assertEqual(serializer_data[0]['max_rating'], None)
 
     def test_get_filter(self):
         url = reverse('paint-list')
-        paints = Paint.objects.filter(id=self.paint_2.id).annotate(average_rating=Avg('userpaint__rating'),
-                                                                   max_rating=Max('userpaint__rating')).order_by('id')
+        paints = (
+            Paint.objects.filter(id=self.paint_2.id)
+            .annotate(
+                average_rating=Avg('userpaint__rating'),
+                max_rating=Max('userpaint__rating'),
+            )
+            .order_by('id')
+        )
         response = self.client.get(url, data={'published': 0})
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -67,15 +77,17 @@ class PaintsApiTestCase(APITestCase):
 
     def test_get_search(self):
         url = reverse('paint-list')
-        paints = Paint.objects.filter(id__in=[self.paint_1.id, self.paint_3.id]).annotate(
+        paints = Paint.objects.filter(
+            id__in=[self.paint_1.id, self.paint_3.id]
+        ).annotate(
             average_rating=Avg('userpaint__rating'),
-            max_rating=Max('userpaint__rating'))
+            max_rating=Max('userpaint__rating'),
+        )
         response = self.client.get(url, data={'search': '600.00.00'})
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(
-            PaintSerializer(paints, many=True).data,
-            response.data
+            PaintSerializer(paints, many=True).data, response.data
         )
 
     # CRUD Tests
@@ -91,7 +103,9 @@ class PaintsApiTestCase(APITestCase):
         # 3 items were created during setup
         self.assertEqual(3, Paint.objects.all().count())
         self.client.force_login(self.user)
-        response = self.client.post(url, data=json_data, content_type='application/json')
+        response = self.client.post(
+            url, data=json_data, content_type='application/json'
+        )
 
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         # 4th item created
@@ -106,7 +120,9 @@ class PaintsApiTestCase(APITestCase):
         json_data = json.dumps(data)
 
         self.client.force_login(self.user)
-        response = self.client.put(url, data=json_data, content_type='application/json')
+        response = self.client.put(
+            url, data=json_data, content_type='application/json'
+        )
 
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.paint_1.refresh_from_db()
@@ -124,7 +140,8 @@ class PaintsApiTestCase(APITestCase):
 
     def test_update_not_owner(self):
         # Test for updating the paint if user is not the owner of this paint
-        # Test returns 403 error, and it's ok because not owner shouldn't change the paint
+        # Test returns 403 error, and it's ok because not owner shouldn't
+        # change the paint
         self.user2 = User.objects.create(username='test_username_2')
         url = reverse('paint-detail', args=(self.paint_1.id,))
         data = {
@@ -133,7 +150,9 @@ class PaintsApiTestCase(APITestCase):
         json_data = json.dumps(data)
 
         self.client.force_login(self.user2)
-        response = self.client.put(url, data=json_data, content_type='application/json')
+        response = self.client.put(
+            url, data=json_data, content_type='application/json'
+        )
         print(response.data)
 
         self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
@@ -149,7 +168,9 @@ class PaintsApiTestCase(APITestCase):
         json_data = json.dumps(data)
 
         self.client.force_login(self.user_staff)
-        response = self.client.put(url, data=json_data, content_type='application/json')
+        response = self.client.put(
+            url, data=json_data, content_type='application/json'
+        )
 
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.paint_1.refresh_from_db()
@@ -157,7 +178,8 @@ class PaintsApiTestCase(APITestCase):
 
     def test_delete_not_owner(self):
         # Test for deleting the paint if user is not the owner of this paint
-        # Test returns 403 error, and it's ok because not owner shouldn't delete the paint
+        # Test returns 403 error, and it's ok because not owner shouldn't
+        # delete the paint
         self.user2 = User.objects.create(username='test_username_2')
         url = reverse('paint-detail', args=(self.paint_3.id,))
         self.assertEqual(3, Paint.objects.all().count())
@@ -170,7 +192,8 @@ class PaintsApiTestCase(APITestCase):
         self.assertEqual(3, Paint.objects.all().count())
 
     def test_delete_not_owner_but_staff(self):
-        # Test for deleting the paint if user is not the owner but has is_staff flag
+        # Test for deleting the paint if user is not the owner but has
+        # is_staff flag
         url = reverse('paint-detail', args=(self.paint_3.id,))
         self.assertEqual(3, Paint.objects.all().count())
 
@@ -184,9 +207,13 @@ class PaintsApiTestCase(APITestCase):
 class UserPaintsApiTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create(username='test_username')
-        self.user_staff = User.objects.create(username='admin_user', is_staff=True)
+        self.user_staff = User.objects.create(
+            username='admin_user', is_staff=True
+        )
         manufacturer = Manufacturer.objects.create(name='Test Manufacturer 1')
-        series = Series.objects.create(name='Test Series 1', manufacturer=manufacturer)
+        series = Series.objects.create(
+            name='Test Series 1', manufacturer=manufacturer
+        )
         self.paint_1 = Paint.objects.create(
             name='Test 1',
             catalog_number='600.00.00',
@@ -218,7 +245,9 @@ class UserPaintsApiTestCase(APITestCase):
         }
         json_data = json.dumps(data)
         self.client.force_login(self.user)
-        response = self.client.patch(url, data=json_data, content_type='application/json')
+        response = self.client.patch(
+            url, data=json_data, content_type='application/json'
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         user_paint = UserPaint.objects.get(user=self.user, paint=self.paint_1)
         self.assertTrue(user_paint.favorite)
@@ -232,7 +261,9 @@ class UserPaintsApiTestCase(APITestCase):
         }
         json_data = json.dumps(data)
         self.client.force_login(self.user)
-        response = self.client.patch(url, data=json_data, content_type='application/json')
+        response = self.client.patch(
+            url, data=json_data, content_type='application/json'
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         user_paint = UserPaint.objects.get(user=self.user, paint=self.paint_1)
         self.assertEqual(4, user_paint.rating)
@@ -242,26 +273,34 @@ class UserPaintsApiTestCase(APITestCase):
             'rating': 1000,
         }
         json_data = json.dumps(data)
-        response = self.client.patch(url, data=json_data, content_type='application/json')
+        response = self.client.patch(
+            url, data=json_data, content_type='application/json'
+        )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         data = {
             'rating': 'test',
         }
         json_data = json.dumps(data)
-        response = self.client.patch(url, data=json_data, content_type='application/json')
+        response = self.client.patch(
+            url, data=json_data, content_type='application/json'
+        )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         data = {
             'rating': -400,
         }
         json_data = json.dumps(data)
-        response = self.client.patch(url, data=json_data, content_type='application/json')
+        response = self.client.patch(
+            url, data=json_data, content_type='application/json'
+        )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         data = {
             'rating': 0,
         }
         json_data = json.dumps(data)
-        response = self.client.patch(url, data=json_data, content_type='application/json')
+        response = self.client.patch(
+            url, data=json_data, content_type='application/json'
+        )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
