4cb17983558a86602aef8d4b4ca9a21f6f28539a
roman <roman.petrakov+git@gmail.com>
2022-06-03 23:27:17 +0300

    renamed core project folder, added CORS headers, renamed requirements.txt

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-"""modelpaints URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -18,7 +18,7 @@ from django.urls import path, include, re_path
 
 from rest_framework.routers import SimpleRouter
 
-from modelpaints import settings
+from core import settings
 from warehouse.views import PaintsViewSet, auth, UserPaintViewSet
 
 router = SimpleRouter()
